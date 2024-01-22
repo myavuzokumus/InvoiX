9915ac912e051908e84f0b18bec28b2d2693122d
Over_Brave <47111280+OverBrave@users.noreply.github.com>
2024-01-22 22:27:17 +0300

    Starting make to modular code base

--- lib/Pages/captured_page.dart
+++ lib/Pages/captured_page.dart
@@ -11,6 +11,8 @@ import 'package:string_similarity/string_similarity.dart';
 import '../Models/invoice_data.dart';
 import '../company_name_filter.dart';
 import '../toast.dart';
+import '../utils/image_to_text_regex.dart';
+import '../widgets/warn_icon.dart';
 
 class InvoiceCaptureScreen extends StatefulWidget {
   const InvoiceCaptureScreen(
@@ -42,43 +44,12 @@ class _InvoiceCaptureScreenState extends State<InvoiceCaptureScreen> {
   //TextLabelStyle
   InputDecoration textFieldDecoration(final text, final message) =>
       InputDecoration(
-          border: const OutlineInputBorder(),
-          isDense: true,
           labelText: text,
-          labelStyle:
-              const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-          counterStyle: const TextStyle(fontSize: 0),
-          errorStyle: const TextStyle(fontSize: 0),
-          suffixIcon: Tooltip(
-              triggerMode: TooltipTriggerMode.tap,
-              showDuration: const Duration(seconds: 3),
-              message: message,
-              child: const Icon(
-                Icons.info_outline,
-                size: 24,
-              )));
+          suffixIcon: WarnIcon(message: message)
+      );
 
   //TODO: Company name selection screen will be added.
   //TODO: Invoice Type detection will be added.
-
-  //Regex
-  //RegExp NameRegex = RegExp(r"\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+", caseSensitive: false);
-
-  // I need to find a different solution here, because it is not working perfectly.
-  // I inspected some similar projects and they are working perfectly but they are not open source. They probably using OCR.
-  // I am open to contributions.
-  // Google ML Text Recognition is not working perfectly, so it can’t read everything properly.
-  // OCR can be used here, but there are a lot of projects already available (GCS also has invoice recognition but it needs a price to use).
-  // So I wanna make my own parser or reader for the best use of Google ML Text Recognition.
-
-  final RegExp companyRegex =
-      RegExp(r"(?:LTD\.|ŞT(İ|Í)\.|A\.Ş\.|A\.S\.)", caseSensitive: false);
-  final RegExp dateRegex = RegExp(
-      r"(0[1-9]|[12][0-9]|3[01])(\/|-)(0[1-9]|1[1,2])(\/|-)(19|20)\d{2}",
-      caseSensitive: false);
-  final RegExp amountRegex = RegExp(
-      r"^(\$|\₺|€)(0|[1-9][0-9]{0,2})(,\d{1,4})*(\.\d{1,2})?$|^(0|[1-9][0-9]{0,2})(,\d{1,4})*(\.\d{1,2})?(\$|\₺| TL|TL|€)$",
-      caseSensitive: false);
 
   @override
   void initState() {
