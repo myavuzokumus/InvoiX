5a2a09cbf63cd655825f300599c1d37497df320c
Over_Brave <47111280+OverBrave@users.noreply.github.com>
2024-02-07 00:28:38 +0300

    Text color change to white.

--- lib/widgets/toast.dart
+++ lib/widgets/toast.dart
@@ -4,7 +4,7 @@ void showSnackBar(final BuildContext context,
     {required final String text, final Color color = Colors.deepOrangeAccent}) {
   ScaffoldMessenger.of(context).hideCurrentSnackBar();
   final snackBar = SnackBar(
-    content: Text(text),
+    content: Text(text, style: const TextStyle(color: Colors.white)),
     backgroundColor: color,
     behavior: SnackBarBehavior.floating,
     margin: const EdgeInsets.all(50),
