b52e9617e590bcda0c2d39097981ab53e861867b
Over_Brave <47111280+OverBrave@users.noreply.github.com>
2024-02-07 20:14:56 +0300

    InvoiceCard Widget added.

---
+++ lib/widgets/invoice_card.dart
@@ -0,0 +1,69 @@
+import 'dart:io';
+
+import 'package:cross_file/cross_file.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:invoix/models/invoice_data.dart';
+import 'package:invoix/pages/invoice_edit.dart';
+
+class InvoiceCard extends StatelessWidget {
+  const InvoiceCard({super.key, required this.invoiceData, required this.index});
+
+  final InvoiceData invoiceData;
+  final int index;
+
+  @override
+  Widget build(final BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (final context) =>
+                      InvoiceCaptureScreen(
+                        editIndex: index,
+                        imageFile: XFile(invoiceData.ImagePath),
+                      )));
+        },
+        splashColor: Colors.blue,
+        borderRadius: BorderRadius.circular(16),
+        child: Ink(
+          padding: const EdgeInsets.only(left: 16),
+          decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(16)),
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                stops: [0.3, 0.5, 0.7],
+                colors: [ Color(0xFF846AFF), Color(0xFF755EE8), Color(0xFF846AFF)],
+              ),
+          ),// Adds a gradient background and rounded corners to the container
+          child: Row(
+            children: [
+              Expanded(
+                child: ListView(
+                  shrinkWrap: true,
+                  children: <Widget>[
+                    Text("Invoice No\n${invoiceData.invoiceNo}"),
+                    const Divider(height: 2),
+                    Text("Date\n${DateFormat("dd-MM-yyyy").format(invoiceData.date)}"),
+                    const Divider(height: 2),
+                    Text("Amount\n${invoiceData.amount}"),
+                  ],
+                ),
+              ),
+              Hero(
+                tag: invoiceData.ImagePath,
+                child: Image.file(File(
+                    XFile(invoiceData.ImagePath).path),
+                width: 144,),
+              ), // Adds a price to the bottom of the card
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
