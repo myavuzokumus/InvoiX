ea411c1ad25070b641a51b5b6627896931663c93
Over_Brave <47111280+OverBrave@users.noreply.github.com>
2024-02-07 20:14:31 +0300

    InvoiceList moved to widget.

--- lib/pages/invoice_list.dart
+++ lib/pages/invoice_list.dart
@@ -1,15 +1,11 @@
-import 'dart:io';
-
-import 'package:file_selector/file_selector.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:intl/intl.dart';
+import 'package:invoix/models/invoice_data.dart';
+import 'package:invoix/widgets/invoice_card.dart';
 
 import '../main.dart';
-import '../models/invoice_data.dart';
 import '../utils/company_name_filter.dart';
 import '../widgets/toast.dart';
-import 'invoice_edit.dart';
 
 class InvoicePage extends StatefulWidget {
   const InvoicePage({super.key, required this.companyName});
@@ -44,11 +40,14 @@ class _InvoicePageState extends State<InvoicePage> {
           actions: <Widget>[
             IconButton(
               icon: const Icon(Icons.table_chart),
-              tooltip: 'Export all data to Excel',
+              tooltip: "Export all data to Excel",
               onPressed: () => showSnackBar(context,
-                  text: "Files are saved in " "Download" " file."),
+                  text: "Excel files are saved in " "Download" " file.",
+                  color: Colors.green),
             ),
           ]),
+
+
       body: InvoiceList(companyName: companyName),
     );
   }
@@ -70,92 +69,38 @@ class InvoiceList extends StatelessWidget {
                 invoiceDataBox.cast<InvoiceData>(), companyName),
             builder: (final BuildContext context,
                 final AsyncSnapshot<List<InvoiceData>> invoice) {
-              if (invoice.hasData) {
-                return GridView.builder(
-                  // Create a grid with 2 columns. If you change the scrollDirection to
-                  // horizontal, this produces 2 rows.
-                  // Generate 100 widgets that display their index in the List.
-                  gridDelegate:
-                  const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 1,
-                    mainAxisSpacing: 15,
-                    crossAxisSpacing: 15,
-                    childAspectRatio: 0.9,
-                  ),
-                  itemCount: invoice.data!.length,
-                  itemBuilder: (final BuildContext context, final int index) {
-                    final invoiceData = invoice.data!.elementAt(index);
 
-                    return InkWell(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (final context) =>
-                                    InvoiceCaptureScreen(
-                                      editIndex: index,
-                                      imageFile: XFile(invoiceData.ImagePath),
-                                    )));
-                      },
-                      child: ListTile(
-                        title: ClipRRect(
-                          borderRadius: BorderRadius.circular(20.0),
-                          child: Container(
-                            color: Colors.grey,
-                            child: Center(
-                              child: Column(
-                                children: [
-                                  Flexible(
-                                    child: Container(
-                                      margin: const EdgeInsets.all(15),
-                                      color: Colors.blueGrey,
-                                      child: Image.file(File(
-                                          XFile(invoiceData.ImagePath).path)),
-                                    ),
-                                  ),
-                                  ListView(
-                                    shrinkWrap: true,
-                                    children: <Widget>[
-                                      ListTile(
-                                        visualDensity:
-                                        const VisualDensity(vertical: -4),
-                                        title: const Text("Invoice No:",
-                                            style: TextStyle(fontSize: 20)),
-                                        trailing: Text(invoiceData.invoiceNo,
-                                            style: const TextStyle(
-                                                fontSize: 16)),
-                                      ),
-                                      ListTile(
-                                        visualDensity:
-                                        const VisualDensity(vertical: -4),
-                                        title: const Text("Date:",
-                                            style: TextStyle(fontSize: 20)),
-                                        trailing: Text(
-                                            DateFormat("dd-MM-yyyy")
-                                                .format(invoiceData.date),
-                                            style: const TextStyle(
-                                                fontSize: 16)),
-                                      ),
-                                      ListTile(
-                                        visualDensity:
-                                        const VisualDensity(vertical: -4),
-                                        title: const Text("Amount:",
-                                            style: TextStyle(fontSize: 20)),
-                                        trailing: Text(
-                                            invoiceData.amount.toString(),
-                                            style: const TextStyle(
-                                                fontSize: 16)),
-                                      ),
-                                    ],
-                                  )
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
+              if (invoice.hasData) {
+
+                final List<InvoiceData> invoiceList = invoice.data!;
+
+                return Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 10, top: 10),
+                      child: FilledButton(
+                        onPressed: () {},
+                        child: Text(invoiceList.length.toString()),
                       ),
-                    );
-                  },
+                    ),
+                    GridView.builder(
+                      shrinkWrap: true,
+                      padding: const EdgeInsets.only(left: 20, right: 20),
+                      gridDelegate:
+                      const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 1,
+                        mainAxisSpacing: 15,
+                        crossAxisSpacing: 15,
+                        childAspectRatio: 2.60,
+                      ),
+                      itemCount: invoiceList.length,
+                      itemBuilder: (final BuildContext context, final int index) {
+
+                        return InvoiceCard(invoiceData: invoiceList.elementAt(index), index: index);
+
+                      },
+                    ),
+                  ],
                 );
               } else {
                 return const CircularProgressIndicator();
