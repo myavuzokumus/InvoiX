ad1aae141818bac198fabab4fe97eb1b980a99a0
Over_Brave <47111280+OverBrave@users.noreply.github.com>
2024-02-12 20:23:48 +0300

    Little fix

--- lib/utils/image_to_text_regex.dart
+++ lib/utils/image_to_text_regex.dart
@@ -9,7 +9,7 @@
 // So I wanna make my own parser or reader for the best use of Google ML Text Recognition.
 
 final RegExp companyRegex =
-    RegExp(r"(?:LTD\.|ŞTİ\.|A\.Ş\.|LLC|PLC|INC|GMBH)", caseSensitive: false);
+    RegExp(r"(?:LTD\.|ŞTİ\.|(A\.Ş\.|A\.S\.)|LLC|PLC|INC|GMBH)", caseSensitive: false);
 
 final RegExp dateRegex = RegExp(
     r"(0[1-9]|[12][0-9]|3[01])(\/|-|\.)(0[1-9]|1[1,2])(\/|-|\.)(19|20)\d{2}",
