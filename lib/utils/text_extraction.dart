f3acffc97a72ee077f3976bff9e2b31ce8d1e6e2
Over_Brave <47111280+OverBrave@users.noreply.github.com>
2024-02-12 18:39:26 +0300

    Multiple threads (Isolate) were used for optimization.

--- lib/utils/text_extraction.dart
+++ lib/utils/text_extraction.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:cross_file/cross_file.dart';
+import 'package:flutter/foundation.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 import 'package:image/image.dart' as img;
 import 'package:path_provider/path_provider.dart';
@@ -9,22 +10,34 @@ final TextRecognizer textRecognizer = TextRecognizer(script: TextRecognitionScri
 
 Future<List<String>> getScannedText(final XFile image) async {
 
-  final img.Image adjustedImage = img.decodeImage(File(image.path).readAsBytesSync())!;
+  img.Image adjustedImage = img.decodeImage(await File(image.path).readAsBytes())!;
+
+  const int newWidth = 512;
+  final int newHeight = (adjustedImage.height * newWidth) ~/ adjustedImage.width; // Oranı koruyun
+
+  adjustedImage = img.copyResize(adjustedImage, width: newWidth, height: newHeight);
 
   img.smooth(adjustedImage, weight: 3);
   img.adjustColor(adjustedImage, brightness: 1.17);
 
   final Directory tempDir = await getTemporaryDirectory();
   final File imageFile = await File('${tempDir.path}/image.jpg').create();
-  imageFile.writeAsBytesSync(img.encodePng(adjustedImage));
 
-  final inputImage = InputImage.fromFile(imageFile);
+  final pngBytes = await compute(img.encodePng, adjustedImage);
+  await imageFile.writeAsBytes(pngBytes);
+
+  final inputImage = InputImage.fromFile(File(image.path));
   final RecognizedText extractedText =
   await textRecognizer.processImage(inputImage);
   await textRecognizer.close();
+  print("c");
 
   //For test
   print(extractedText.text.split("\n"));
+
+  await Future.delayed(Duration(seconds: 5));
+
+  throw UnimplementedError();
 
   return extractedText.text.split("\n");
 
