e78a202864c52a8755e0e8343b4c2e146e1f2ea6
Over_Brave <47111280+OverBrave@users.noreply.github.com>
2024-02-15 21:39:31 +0300

    Cooldown added for constant ai analyze.

--- lib/widgets/ai_button.dart
+++ lib/widgets/ai_button.dart
@@ -1,8 +1,20 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:invoix/models/invoice_analysis.dart';
+import 'package:invoix/utils/ai/geminiAPI.dart';
+import 'package:invoix/utils/ai/prompts.dart';
 import 'package:invoix/widgets/loading_animation.dart';
+import 'package:invoix/widgets/toast.dart';
+import 'package:invoix/widgets/warn_icon.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AIButton extends StatelessWidget {
-  const AIButton({super.key});
+  const AIButton({super.key, required this.invoiceImage});
+
+  final File invoiceImage;
 
   @override
   Widget build(final BuildContext context) {
@@ -11,38 +23,213 @@ class AIButton extends StatelessWidget {
         backgroundColor: Colors.black.withOpacity(0.35),
         side: const BorderSide(width: 1.5, color: Colors.orangeAccent),
       ),
-      onPressed: () {
-        showModalBottomSheet<void>(
-          showDragHandle: true,
-          context: context,
-          builder: (final BuildContext context) {
-            return SizedBox(
-              height: 200,
-              width: double.infinity,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 24, bottom: 24, right: 24),
-                child: LayoutBuilder(
-                  builder: (final BuildContext context, final BoxConstraints constraints) {
-                    return Card(elevation: 16, child: SingleChildScrollView(
-                      child: Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: Column(
-                          children: [
-                            LoadingAnimation(customHeight: constraints.maxHeight - 72),
-                            const Text('Very soon, it will be possible to analyze invoices using AI.', textAlign: TextAlign.center),
-                          ],
-                        ),
-                      ),
-                    ));
-                  },
+      onPressed: () async {
+        final SharedPreferences prefs = await SharedPreferences.getInstance();
+        int remainingTime = prefs.getInt(invoiceImage.path) ?? 0;
+
+        if (remainingTime == 0) {
+          Timer.periodic(const Duration(seconds: 1), (final t) async {
+            remainingTime += 1;
+
+            if (remainingTime >= 30) {
+              remainingTime = 0;
+              t.cancel();
+            }
+            await prefs.setInt(invoiceImage.path, remainingTime);
+          });
+
+          showModalBottomSheet<void>(
+            showDragHandle: true,
+            context: context,
+            builder: (final BuildContext context) {
+              return SizedBox(
+                height: 425,
+                width: double.infinity,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.only(left: 24, bottom: 24, right: 24),
+                  child: LayoutBuilder(
+                    builder: (final BuildContext context,
+                        final BoxConstraints constraints) {
+                      return Card(
+                          elevation: 16,
+                          child: Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: FutureBuilder(
+                              future: GeminiAPI().describeImage(
+                                  imgFile: invoiceImage,
+                                  prompt: describeInvoicePrompt),
+                              builder: (final BuildContext context,
+                                  final AsyncSnapshot<String> snapshot) {
+                                if (snapshot.hasData) {
+                                  final Map<String, dynamic> decodedData =
+                                      jsonDecode(snapshot.data!
+                                          .replaceAll("```", "")
+                                          .replaceFirst("json", ""));
+                                  final InvoiceAnalysis invoiceAnalysis =
+                                      InvoiceAnalysis.fromJson(decodedData);
+
+                                  return describedWidget(invoiceAnalysis);
+                                } else if (snapshot.hasError) {
+                                  return const Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      Icon(Icons.phonelink_erase_rounded,
+                                          size: 92, color: Colors.red),
+                                      Text('No Internet Connection',
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                              fontSize: 24,
+                                              fontWeight: FontWeight.bold)),
+                                    ],
+                                  );
+                                }
+                                return Column(
+                                  children: [
+                                    LoadingAnimation(
+                                        customHeight:
+                                            constraints.maxHeight - 72),
+                                    const Text(
+                                        'Your invoice is being analyzed...',
+                                        textAlign: TextAlign.center),
+                                  ],
+                                );
+                              },
+                            ),
+                          ));
+                    },
+                  ),
                 ),
-              ),
-            );
-          },
-        );
+              );
+            },
+          );
+        } else {
+          toast(context,
+              text:
+                  "Please wait ${30 - remainingTime} seconds before opening the sheet again.");
+        }
       },
       icon: const Text("✨", style: TextStyle(fontSize: 17)),
       tooltip: 'Analyze it with AI',
+    );
+  }
+
+  //This is ridiculous, I know, but I did it this way to improve the application.
+  Widget describedWidget(final InvoiceAnalysis invoiceAnalysis) {
+    return ListView(
+      children: [
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("Purchased products",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.left),
+            WarnIcon(
+                message:
+                    "The information provided may sometimes be incorrect. Please take this into consideration and pay attention to the recommendations.")
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 12),
+          child: RichText(
+            text: TextSpan(
+              children: invoiceAnalysis.whichItemsBought
+                  .map((final e) => TextSpan(
+                        text: "● ${e.name}\n",
+                        style: const TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.bold),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: "- ${e.price}\n",
+                            style: const TextStyle(
+                                fontSize: 14, fontWeight: FontWeight.normal),
+                          ),
+                        ],
+                      ))
+                  .toList(),
+            ),
+          ),
+        ),
+        ExpansionTile(
+          initiallyExpanded: true,
+          title: const Text(
+              "Are the products purchased harmful to human health?",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 12, top: 8, bottom: 8),
+              child: Text(invoiceAnalysis.anyHealthProblem,
+                  style: const TextStyle(fontSize: 14)),
+            ),
+          ],
+        ),
+        ExpansionTile(
+          title: const Text(
+              "Are the purchased products harmful to the environment?",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 12, top: 8, bottom: 8),
+              child: Text(invoiceAnalysis.anyHabitatProblem,
+                  style: const TextStyle(fontSize: 14)),
+            ),
+          ],
+        ),
+        ExpansionTile(
+          title: const Text(
+              "What are the alternatives to the products purchased?",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 12, top: 8, bottom: 8),
+              child: RichText(
+                text: TextSpan(
+                  children: invoiceAnalysis.alternatives
+                      .map((final e) => TextSpan(
+                            text: "● ${e.name}\n",
+                            style: const TextStyle(
+                                fontSize: 14, fontWeight: FontWeight.bold),
+                            children: <TextSpan>[
+                              TextSpan(
+                                text: "- ${e.description}\n",
+                                style: const TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.normal),
+                              ),
+                            ],
+                          ))
+                      .toList(),
+                ),
+              ),
+            ),
+          ],
+        ),
+        ExpansionTile(
+          title: const Text(
+              "What can be suggested for more conscious consumption?",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 12, top: 8, bottom: 8),
+              child: Text(invoiceAnalysis.consciousConsumption,
+                  style: const TextStyle(fontSize: 14)),
+            ),
+          ],
+        ),
+        ExpansionTile(
+          title: const Text(
+              "What is the market value of the products purchased? How has this value changed in the last year?",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 12, top: 8, bottom: 8),
+              child: Text(invoiceAnalysis.marketResearch,
+                  style: const TextStyle(fontSize: 14)),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
