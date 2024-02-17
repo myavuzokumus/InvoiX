8adc5fe9d6abc04a25f1f4072092fc45799ff39a
Over_Brave <47111280+OverBrave@users.noreply.github.com>
2024-02-17 03:12:18 +0300

    InvoiceData actions gathered together.

---
+++ lib/utils/invoice_data_service.dart
@@ -0,0 +1,66 @@
+import 'package:invoix/main.dart';
+import 'package:invoix/models/invoice_data.dart';
+
+enum ListType { company, invoice }
+
+enum CompanyType { AS, LTD, STI, LLC, PLC, INC, GMBH }
+
+extension CompanyTypeExtension on CompanyType {
+  String get name {
+    switch (this) {
+      case CompanyType.AS:
+        return 'A.Ş.';
+      case CompanyType.LTD:
+        return 'Ltd.';
+      case CompanyType.STI:
+        return 'Şti.';
+      case CompanyType.LLC:
+        return 'LLC';
+      case CompanyType.PLC:
+        return 'PLC';
+      case CompanyType.INC:
+        return 'Inc.';
+      case CompanyType.GMBH:
+        return 'GmbH';
+    }
+  }
+}
+
+mixin InvoiceDataService {
+
+  static Future<void> saveInvoiceData(final InvoiceData invoiceData) async {
+    await invoiceDataBox.put(invoiceData.id, invoiceData);
+  }
+
+  static Future<void> deleteInvoiceData(final InvoiceData invoiceData) async {
+    await invoiceDataBox.delete(invoiceData.id);
+  }
+
+  static Future<List<InvoiceData>> getInvoiceList(final String companyName) async {
+
+    final Iterable<InvoiceData> savedList = invoiceDataBox.values.cast<InvoiceData>();
+
+    return savedList
+        .where((final element) => companyName == element.companyName)
+        .toList();
+  }
+
+  static Future<List<String>> getCompanyList() async {
+    final Iterable<InvoiceData> savedList = invoiceDataBox.values.cast<InvoiceData>();
+    return savedList.map((final item) => item.companyName).toSet().toList();
+  }
+}
+
+
+//  static Future<List<InvoiceData>> getCompanyList() async {
+//
+//     final Iterable<InvoiceData> savedList = invoiceDataBox.values.cast<InvoiceData>();
+//     List<InvoiceData> returnList = [];
+//
+//     for (final element in savedList) {
+//       if (!returnList.any((final item) => item.companyName == element.companyName)) {
+//         returnList.add(element);
+//       }
+//     }
+//     return returnList;
+//   }
