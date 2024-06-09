1f6bcb90193394c925968d738c2202aa72de0a05
Mustafa Yavuz Okumuş <47111280+myavuzokumus@users.noreply.github.com>
2024-06-09 15:46:08 +0300

    Progress Indicator added to Toast Widget

--- lib/widgets/toast.dart
+++ lib/widgets/toast.dart
@@ -3,15 +3,63 @@ import 'package:flutter/material.dart';
 void Toast(final BuildContext context,
     {required final String text, final Color color = Colors.deepOrangeAccent}) {
 
+  const duration = Duration(milliseconds: 10000);
+
   ScaffoldMessenger.of(context).hideCurrentSnackBar();
   final snackBar = SnackBar(
-    content: Text(text, style: const TextStyle(color: Colors.white)),
+    content: ToastContent(text: text, duration: duration),
     backgroundColor: color,
     behavior: SnackBarBehavior.floating,
     margin: const EdgeInsets.all(50),
     elevation: 30,
-    duration: const Duration(milliseconds: 10000),
-    showCloseIcon: true,
+    duration: duration,
+    padding: const EdgeInsets.all(0)
   );
   ScaffoldMessenger.of(context).showSnackBar(snackBar);
+}
+
+class ToastContent extends StatefulWidget {
+  final String text;
+  final Duration duration;
+
+  const ToastContent({Key? key, required this.text, required this.duration}) : super(key: key);
+
+  @override
+  _ToastContentState createState() => _ToastContentState();
+}
+
+class _ToastContentState extends State<ToastContent> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: widget.duration,
+    )..forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Text(widget.text, style: const TextStyle(color: Colors.white)),
+        ),
+        AnimatedBuilder(animation: _controller,
+        builder: (BuildContext context, Widget? child) => LinearProgressIndicator(value: 1.0 - _controller.value)),
+      ],
+    );
+  }
 }
