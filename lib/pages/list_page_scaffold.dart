df569e385c5a1c5d4a60dba84a5376525bca7776
Mustafa Yavuz Okumuş <47111280+myavuzokumus@users.noreply.github.com>
2024-06-12 00:27:24 +0300

    Summary page added.

--- lib/pages/list_page_scaffold.dart
+++ lib/pages/list_page_scaffold.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:invoix/misc/deletion_dialog.dart';
 import 'package:invoix/models/selection_state.dart';
+import 'package:invoix/pages/SummaryPage/summary_main.dart';
 import 'package:invoix/utils/export_to_excel.dart';
 import 'package:invoix/utils/invoice_data_service.dart';
 import 'package:invoix/widgets/search_bar.dart';
@@ -38,7 +39,7 @@ class _GeneralPageState extends ConsumerState<ListPageScaffold> with _ListPageSc
       },
       child: Scaffold(
         appBar: AppBar(
-          centerTitle: widget.type != ListType.company ? true : false,
+          centerTitle: widget.type != ListType.company,
           title: selectionState.isSelectionMode || widget.type != ListType.company ? RichText(
             textAlign: TextAlign.center,
             text: TextSpan(
@@ -87,6 +88,16 @@ class _GeneralPageState extends ConsumerState<ListPageScaffold> with _ListPageSc
               ),
             ]
             //else add Icons for other actions
+            else
+              ...[
+                IconButton(
+                  icon: const Icon(Icons.pie_chart),
+                  tooltip: "Summary",
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (final BuildContext context) => const SummaryMain()));
+                  },
+                ),
+              ]
           ],
         ),
         body: widget.body,
