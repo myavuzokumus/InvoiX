9d74a63b295cf256794b913a37dddf3b01d4c495
Over_Brave <47111280+OverBrave@users.noreply.github.com>
2024-09-15 19:06:19 +0300

    loading animation effect changed.

--- lib/widgets/loading_animation.dart
+++ lib/widgets/loading_animation.dart
@@ -36,14 +36,13 @@ class NewWidget extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         const LinearProgressIndicator(),
-        Image.asset("assets/loading/InvoiceReadLoading.gif"),
+        Image.asset("assets/loading/InvoiceReadLoading.gif", height: MediaQuery.of(context).size.height / 5),
         Column(
           children: [
-            Text(errorState.errorMessage, style: const TextStyle(color: Colors.red), textAlign: TextAlign.center),
             if (!errorState.subs && subsControl) const Text("Please check your subscription status!", style: TextStyle(color: Colors.red, fontSize: 32, fontWeight: FontWeight.bold), textAlign: TextAlign.center),
+            Text(errorState.errorMessage, style: const TextStyle(color: Colors.red), textAlign: TextAlign.center),
           ],
         ),
-
       ],
     );
   }
