a1241ce738be513bc489db11ea6fab295c7caa45
Mustafa Yavuz Okumuş <47111280+myavuzokumus@users.noreply.github.com>
2024-09-22 13:22:15 +0300

    States are separated.

--- lib/states/firebase_state.dart
+++ lib/states/firebase_state.dart
@@ -1,20 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:invoix/services/firebase_service.dart';
-import 'package:invoix/services/hive_service.dart';
-import 'package:invoix/services/in_app_purchase_service.dart';
 
 final firebaseServiceProvider = Provider<FirebaseService>((final ref) {
   return FirebaseService();
-});
-
-final hiveServiceProvider = Provider<HiveService>((final ref) {
-  return HiveService();
-});
-
-final inAppPurchaseServiceProvider = Provider<InAppPurchaseService>((final ref) {
-  final firebaseService = ref.watch(firebaseServiceProvider);
-  return InAppPurchaseService(firebaseService);
 });
 
 final authStateProvider = StreamProvider<User?>((final ref) {
