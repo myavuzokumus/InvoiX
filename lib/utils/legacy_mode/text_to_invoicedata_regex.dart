97ec042235ca589500f69025fef1c79f766960e8
Mustafa Yavuz Okumuş <47111280+myavuzokumus@users.noreply.github.com>
2024-10-20 20:24:56 +0300

    Date regex updated.

--- lib/utils/legacy_mode/text_to_invoicedata_regex.dart
+++ lib/utils/legacy_mode/text_to_invoicedata_regex.dart
@@ -14,7 +14,7 @@ final RegExp companyRegex =
 final RegExp invoiceNoRegex = RegExp(r'NO\s*:\s*(\S+)', caseSensitive: false);
 
 final RegExp dateRegex = RegExp(
-    r"(0[1-9]|[12][0-9]|3[01])(\/|-|\.)(0[1-9]|1[1,2])(\/|-|\.)(19|20)\d{2}",
+    r"((?:(?:\d{4}|\d{2})[-/.](0[1-9]|1[0-2])[-/.](0[1-9]|[12][0-9]|3[01])|(?:0[1-9]|[12][0-9]|3[01])[-/.](?:0[1-9]|1[0-2])[-/.](?:\d{4}|\d{2})|(?:0[1-9]|1[0-2])[-/.](?:0[1-9]|[12][0-9]|3[01])[-/.](?:\d{4}|\d{2})))(?!\d)",
     caseSensitive: false);
 
 final RegExp amountRegex = RegExp(
